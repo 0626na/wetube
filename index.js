52e822ded54c7cc9f59318aa8351c876f17c91fa
Jin <0626na@gmail.com>
2020-11-03 13:37:41 +0900

    #2.1~#2.6 까지의 내용

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hi");
+import express from "express";
+const app = express();
+const PORT=4000;
+
+const handleListening=() => console.log(`Listening on: http://localhost:${PORT}`);
+
+
+const handleHome=(req,res) => res.send('hello from nills');
+
+
+const handleProfile=(req,res)=> res.send("you are an my profile");
+
+const betweenHome= (req,res,next) => {
+    console.log("Between");
+    next();
+}
+
+app.use(betweenHome);
+
+app.get("/", handleHome);
+
+app.get("/profile",handleProfile);
+
+app.listen(PORT,handleListening);  
