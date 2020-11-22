78a9c98d7c5f47af699033c44b25323529953d6a
Jin <0626na@gmail.com>
2020-11-22 21:33:09 +0900

    #2.25 까지의 내용

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,8 +1,36 @@
-export const join=(req,res) => res.render("Join",{pageTitle:'Join'});
-export const login=(req,res) => res.render("Login",{pageTitle:'Login'});
-export const logout=(req,res) => res.render("Logout",{pageTitle:'Logout'});
+import routes from "../routes";
+
+export const getJoin=(req,res) => {
+    res.render("join",{pageTitle:'Join'});
+};
+
+
+export const postJoin=(req,res)=>{ 
+    const {
+    body: {name, email, password, password2}
+    }=req;
+    if(password!==password2)
+    {
+        res.status(400);
+        res.render("join",{pageTitle:'Join'});
+    }
+    else
+    {
+        // Todo: register User
+        // Todo: Log user in
+        res.redirect(routes.home);
+    }
+};
+
+export const getLogin=(req,res) => res.render("login",{pageTitle:'Login'});
+export const postLogin=(req,res)=> {
+    res.redirect(routes.home);
+}
+
+export const logout=(req,res) => {
+    res.redirect(routes.home);
+}
 export const users=(req,res)=>res.render("Users",{pageTitle:'Users'});
 export const userDetail=(req,res)=>res.render("userDetail",{pageTitle:'UserDetail'});
-export const editVideo=(req,res)=>res.render("editVideo",{pageTitle:'editVideo'});
 export const editProfile=(req,res)=>res.render("editProfile",{pageTitle:"Edit Profile"});
 export const changePassword=(req, res)=>res.render("changePassword",{pageTitle:'ChangePassword'});
--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -6,9 +6,19 @@ export const home=(req,res)=>{
 }
 export const search=(req,res)=>{
     const {query:{term:searchingBy}}=req;
-    res.render("Search",{pageTitle:'Search',searchingBy});
+    res.render("search",{pageTitle:'Search', searchingBy, videos});
 }
-export const upload=(req,res)=>res.render("upload",{pageTitle:'Upload'});
-export const videoDetail=(req,res)=>res.render("Video Detail",{pageTitle:'Video Detail'});
-export const editVideo=(req,res)=>res.render("Edit Video",{pageTitle:'Edit Video'});
-export const deleteVideo=(req,res)=>res.render("Delete Video",{pageTitle:'Delete Video'});
+export const getUpload=(req,res)=>res.render("upload",{pageTitle:'Upload'});
+export const postUpload=(req,res)=>{
+    const{
+        body:{
+            file, title, description
+        }
+    }=req;
+    // Todo: Upload and save video
+    res.redirect(routes.videoDetail(324393));
+};
+
+export const videoDetail=(req,res)=>res.render("videoDetail",{pageTitle:'Video Detail'});
+export const editVideo=(req,res)=>res.render("editVideo",{pageTitle:'Edit Video'});
+export const deleteVideo=(req,res)=>res.render("deleteVideo",{pageTitle:'Delete Video'});
